8e34caf1bcd7e33cbe95d2b8d76294f2eafd38ad
IMaeots <indrek160@gmail.com>
2024-01-14 19:17:31 +0200

    Adde basic game logic and UI.

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card11"
+    @State var aiCard = "card14"
+    
+    @State var playerScore = 0
+    @State var aiScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Image("background-wood-cartoon")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(aiCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    VStack {
+                        Text("AI")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(aiScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func deal() {
+        // Randomize the cards
+        var playerCardNum = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardNum)
+        
+        var aiCardNum = Int.random(in: 2...14)
+        aiCard = "card" + String(aiCardNum)
+        
+        // Update the scores
+        if playerCardNum > aiCardNum {
+           // Add 1 to player score
+            playerScore += 1
+        } else if playerCardNum < aiCardNum {
+            // Add 1 to AI score
+            aiScore += 1
+        }
     }
 }
 
